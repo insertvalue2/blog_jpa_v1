df6f379aa134a58a0a49188fd1f27512c9605e1c
kdt4 <insertvaue@naver.com>
2024-09-11 17:13:29 +0900

    PostRepository 추가

---
+++ src/main/java/com/tenco/blog/_domain/post/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.tenco.blog._domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tenco.blog._domain.post.entity.Article;
+
+// @Repository 생략 가능 -> JpaRepository 선언하고 있음 
+public interface PostRepository extends JpaRepository<Article, Integer> {
+    // 기본적인 CRUD 기능을 자동으로 제공
+    // 추가적인 쿼리 메서드도 정의 가능
+}
